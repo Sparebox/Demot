1d50bb4f7f910345c28d4b2facdd1d29f2c2b6ae
Oskari Ojamaa <eeosoj@utu.fi>
2021-02-09 16:07:37 +0200

    10 kertaa nyt

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Versionhallinta toimii!");
+        for(int i = 0;i < 10;i++){
+            System.out.println("Versionhallinta toimii!");
+        }
+        
     }
 
 }
